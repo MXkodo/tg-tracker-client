f4383ab843278ff5f74a460e8b9dddb8bfd611db
MXkodo <nemchinow2018@mail.ru>
2024-07-14 15:52:34 +0400

    Some changes

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ScrollContainer from "./ScrollContainer";
 import "../styles/MainContent.css";
 import ClearIcon from "../img/Clear.png";
@@ -8,6 +9,21 @@ import UpdateIcon from "../img/Update.png";
 
 function MainContent() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Функция для загрузки задач с API
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get("/api/v1/tasks");
+        setTasks(response.data); // Устанавливаем полученные задачи в состояние
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+
+    fetchTasks(); // Вызываем функцию загрузки при монтировании компонента
+  }, []); // Пустой массив зависимостей, чтобы запрос выполнялся один раз при загрузке
 
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
@@ -42,7 +58,15 @@ function MainContent() {
           <img src={SettingIcon} alt="Setting" />
           <img src={UpdateIcon} alt="Update" />
         </div>
-        <p>123</p>
+        <div className="task-tiles">
+          {tasks.map((task) => (
+            <div key={task.id} className="task-tile">
+              <h3>{task.title}</h3>
+              <p>Время отправки: {task.timestamp}</p>
+              <p>Имя группы: {task.groupName}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
