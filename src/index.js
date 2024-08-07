6a8f09778250bde700db391136e4b6a5530f6056
MXkodo <nemchinow2018@mail.ru>
2024-08-07 18:40:53 +0400

    Some changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-const tg = window.Telegram.WebApp;
-
 ReactDOM.render(<App />, document.getElementById("root"));
