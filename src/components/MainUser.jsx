291934cf1e1c948837c135974b1a95d92ca5fb26
MXkodo <nemchinow2018@mail.ru>
2024-08-10 19:56:07 +0400

    Some changes

--- src/components/MainUser.jsx
+++ src/components/MainUser.jsx
@@ -2,15 +2,19 @@ import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import ScrollContainer from "./ScrollContainer";
 import "../styles/MainContent.css";
+import ClearIcon from "../img/Clear.png";
 import UpdateIcon from "../img/Update.png";
 
 function MainContent({ userUUID, userRole }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [allTasks, setAllTasks] = useState([]);
   const [tasks, setTasks] = useState([]);
   const [groups, setGroups] = useState([]);
   const [activeStatusId, setActiveStatusId] = useState(1);
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
 
+  // Мемоизация функции fetchTasks с помощью useCallback
   const fetchTasks = useCallback(async () => {
     try {
       let endpoint = "";
@@ -48,16 +52,17 @@ function MainContent({ userUUID, userRole }) {
           },
         }
       );
-      setTasks(response.data);
+      setAllTasks(response.data);
+      filterTasksByStatus(response.data, activeStatusId);
     } catch (error) {
       console.error("Error fetching tasks:", error);
     }
-  }, [activeStatusId, userUUID]);
+  }, [activeStatusId, userUUID]); // Зависимости функции fetchTasks
 
   useEffect(() => {
     fetchGroups();
     fetchTasks();
-  }, [activeStatusId, userUUID, fetchTasks]);
+  }, [activeStatusId, userUUID, fetchTasks]); // Добавляем fetchTasks в массив зависимостей
 
   const fetchGroups = async () => {
     try {
@@ -101,6 +106,15 @@ function MainContent({ userUUID, userRole }) {
     }
   };
 
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+    filterTasksBySearchAndStatus(allTasks, e.target.value, activeStatusId);
+  };
+
+  const clearSearch = () => {
+    setSearchTerm("");
+  };
+
   const getGroupNameByUUID = (groupUUID) => {
     const group = groups.find((g) => g.uuid === groupUUID);
     return group ? group.name : "Unknown Group";
@@ -108,6 +122,9 @@ function MainContent({ userUUID, userRole }) {
 
   const handleFilterChange = (statusId) => {
     setActiveStatusId(statusId);
+    // Очищаем текущие задачи перед загрузкой новых
+    setTasks([]);
+    fetchTasks();
   };
 
   const handleEditClick = (task) => {
@@ -118,6 +135,91 @@ function MainContent({ userUUID, userRole }) {
   const closeModal = () => {
     setModalOpen(false);
     setSelectedTask(null);
+  };
+
+  const handleSortChange = (e) => {
+    const selectedSortOption = e.target.value;
+    switch (selectedSortOption) {
+      case "group":
+        sortByGroup();
+        break;
+      case "title":
+        sortByTitle();
+        break;
+      case "timestamp":
+        sortByTimestamp();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const sortByGroup = () => {
+    const sortedTasks = [...tasks].sort((a, b) => {
+      const groupNameA = getGroupNameByUUID(a.group_uuid).toLowerCase();
+      const groupNameB = getGroupNameByUUID(b.group_uuid).toLowerCase();
+      return groupNameA.localeCompare(groupNameB);
+    });
+    setTasks(sortedTasks);
+  };
+
+  const sortByTitle = () => {
+    const sortedTasks = [...tasks].sort((a, b) =>
+      a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+    );
+    setTasks(sortedTasks);
+  };
+
+  const sortByTimestamp = () => {
+    const sortedTasks = [...tasks].sort((a, b) => {
+      const timestampA = new Date(a.apperance_timestamp);
+      const timestampB = new Date(b.apperance_timestamp);
+      return timestampB - timestampA;
+    });
+    setTasks(sortedTasks);
+  };
+
+  const filterTasksByStatus = (tasksArray = [], statusId) => {
+    if (!tasksArray) {
+      console.error("Tasks array is null or undefined.");
+      return;
+    }
+    const filteredTasks = tasksArray.filter(
+      (task) => task.status_id === statusId
+    );
+    setTasks(filteredTasks);
+  };
+
+  const filterTasksBySearchAndStatus = (
+    tasksArray = [],
+    searchTerm,
+    statusId
+  ) => {
+    if (!tasksArray) {
+      console.error("Tasks array is null or undefined.");
+      return;
+    }
+
+    let filteredTasks = tasksArray;
+
+    if (statusId !== null) {
+      filteredTasks = filteredTasks.filter(
+        (task) => task.status_id === statusId
+      );
+    }
+
+    if (searchTerm.trim() !== "") {
+      filteredTasks = filteredTasks.filter((task) => {
+        const taskName = task.name || "";
+        const groupName = getGroupNameByUUID(task.group_uuid) || "";
+        return (
+          taskName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          groupName.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      });
+    }
+
+    setTasks(filteredTasks);
   };
 
   return (
@@ -131,6 +233,30 @@ function MainContent({ userUUID, userRole }) {
       </header>
       <div className="flex flex-col flex-grow bg-[#525252] rounded-[17px] overflow-y-auto p-5 box-border mb-4 h-[79vh]">
         <div className="flex items-center mr-0">
+          <input
+            id="search"
+            type="text"
+            placeholder="Поиск..."
+            value={searchTerm}
+            onChange={handleInputChange}
+            className="rounded-[15px] w-[27vh] h-[5vh] p-1"
+            style={{ color: "black" }}
+          />
+
+          {searchTerm && (
+            <button className="clear-button" onClick={clearSearch}>
+              <img src={ClearIcon} alt="Clear" />
+            </button>
+          )}
+
+          <select
+            className="sort-dropdown rounded-[15px] ml-2 h-[5vh] bg-green-500 border-none cursor-pointer text-white font-semibold transition-colors duration-300"
+            onChange={handleSortChange}
+          >
+            <option value="group">По группе</option>
+            <option value="title">По названию</option>
+            <option value="timestamp">По времени</option>
+          </select>
           <button
             className="icon-button rounded-[10px] ml-2 h-[5vh] bg-green-500 border-none cursor-pointer transition-colors duration-300 hover:bg-green-700"
             onClick={refreshData}
@@ -177,26 +303,15 @@ function MainContent({ userUUID, userRole }) {
                     </button>
                   )}
                   {task.status_id === 4 && (
-                    <>
-                      <button
-                        className="accept-button mr-1 px-1 bg-green-500 border-none rounded-lg cursor-pointer text-white font-semibold transition-colors duration-300 hover:bg-green-600"
-                        onClick={(event) => {
-                          event.stopPropagation();
-                          handleAcceptTask(task.uuid, 5);
-                        }}
-                      >
-                        В проверке
-                      </button>
-                      <button
-                        className="needs-work-button mr-1 px-1 bg-orange-500 border-none rounded-lg cursor-pointer text-white font-semibold transition-colors duration-300 hover:bg-orange-700"
-                        onClick={(event) => {
-                          event.stopPropagation();
-                          handleAcceptTask(task.uuid, 6);
-                        }}
-                      >
-                        Доработка
-                      </button>
-                    </>
+                    <button
+                      className="accept-button mr-1 px-1 bg-green-500 border-none rounded-lg cursor-pointer text-white font-semibold transition-colors duration-300 hover:bg-green-600"
+                      onClick={(event) => {
+                        event.stopPropagation();
+                        handleAcceptTask(task.uuid, 5);
+                      }}
+                    >
+                      В проверке
+                    </button>
                   )}
                   {task.status_id === 5 && (
                     <>
@@ -221,84 +336,72 @@ function MainContent({ userUUID, userRole }) {
                     </>
                   )}
                   {task.status_id === 6 && (
-                    <button
-                      className="accept-button mr-1 px-1 bg-green-500 border-none rounded-lg cursor-pointer text-white font-semibold transition-colors duration-300 hover:bg-green-600"
-                      onClick={(event) => {
-                        event.stopPropagation();
-                        handleAcceptTask(task.uuid, 3);
-                      }}
-                    >
-                      Принять
-                    </button>
+                    <>
+                      <button
+                        className="accept-button mr-1 px-1 bg-green-500 border-none rounded-lg cursor-pointer text-white font-semibold transition-colors duration-300 hover:bg-green-600"
+                        onClick={(event) => {
+                          event.stopPropagation();
+                          handleAcceptTask(task.uuid, 3);
+                        }}
+                      >
+                        Принять
+                      </button>
+                    </>
                   )}
                 </>
               )}
             </div>
           </div>
         ))}
+      </div>
+      {modalOpen && selectedTask && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+              <div className="bg-zinc-900 p-5 rounded-lg shadow-lg max-w-4xl max-h-full overflow-auto text-white">
+                <h2 className="text-xl font-bold whitespace-normal overflow-hidden max-w-full">
+                  Информация о задаче
+                </h2>
+                <p className="whitespace-normal overflow-hidden max-w-full">
+                  <strong>Название:</strong> {selectedTask.name}
+                </p>
+                <p className="whitespace-normal overflow-hidden max-w-prose break-words">
+                  <strong>Описание:</strong> {selectedTask.description}
+                </p>
+                <p className="whitespace-normal overflow-hidden max-w-full">
+                  <strong>Группа:</strong>{" "}
+                  {getGroupNameByUUID(selectedTask.group_uuid)}
+                </p>
+                <p className="whitespace-normal overflow-hidden max-w-full">
+                  <strong>Время отправки:</strong>{" "}
+                  {formatTimestamp(selectedTask.apperance_timestamp)}
+                </p>
 
-        {modalOpen && selectedTask && (
-          <div className="modal">
-            <div className="modal-content">
-              <span className="close" onClick={closeModal}>
-                &times;
-              </span>
-              <h2>Редактировать задачу</h2>
-              <form>
-                <label>
-                  Название:
-                  <input
-                    type="text"
-                    value={selectedTask.name}
-                    onChange={(e) =>
-                      setSelectedTask((prev) => ({
-                        ...prev,
-                        name: e.target.value,
-                      }))
-                    }
-                  />
-                </label>
-                <label>
-                  Время:
-                  <input
-                    type="text"
-                    value={formatTimestamp(selectedTask.apperance_timestamp)}
-                    readOnly
-                  />
-                </label>
-                <label>
-                  Группа:
-                  <select
-                    value={selectedTask.group_uuid}
-                    onChange={(e) =>
-                      setSelectedTask((prev) => ({
-                        ...prev,
-                        group_uuid: e.target.value,
-                      }))
-                    }
+                <div className="flex justify-end mt-5">
+                  <button
+                    className="ml-2 px-5 py-2 bg-green-500 border-none rounded-lg cursor-pointer text-white font-semibold transition-colors duration-300 hover:bg-green-600"
+                    onClick={closeModal}
                   >
-                    {groups.map((group) => (
-                      <option key={group.uuid} value={group.uuid}>
-                        {group.name}
-                      </option>
-                    ))}
-                  </select>
-                </label>
-                <button type="button" onClick={closeModal}>
-                  Сохранить
-                </button>
-              </form>
+                    Закрыть
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
 
 const formatTimestamp = (timestamp) => {
   const date = new Date(timestamp);
-  return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
 };
 
 export default MainContent;
