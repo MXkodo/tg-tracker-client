fc87e39247cf7f4896b85f94872d70ae7aa596db
MXkodo <nemchinow2018@mail.ru>
2024-08-17 01:33:50 +0400

    Some changes

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -437,6 +437,8 @@ function MainContent() {
 }
 
 const formatTimestamp = (timestamp) => {
+  if (!timestamp) return "Не указано";
+
   const date = new Date(timestamp);
   const day = date.getDate().toString().padStart(2, "0");
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
