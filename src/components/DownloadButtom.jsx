2e38ed3bc103498123103dd1f088209801501e56
MXkodo <nemchinow2018@mail.ru>
2024-08-20 22:49:35 +0400

    Some changes

--- src/components/DownloadButtom.jsx
+++ src/components/DownloadButtom.jsx
@@ -4,17 +4,24 @@ import axios from "axios";
 const DownloadButton = () => {
   const handleDownload = () => {
     axios({
-      url: "hhttps://taskback.emivn.io/api/v1/tasks/download",
+      url: "https://taskback.emivn.io/api/v1/tasks/download",
       method: "GET",
       responseType: "blob",
-    }).then((response) => {
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", "user_ratings.xlsx");
-      document.body.appendChild(link);
-      link.click();
-    });
+    })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "user_ratings.xlsx");
+        document.body.appendChild(link);
+        link.click();
+        // Очистка ресурсов
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+      })
+      .catch((error) => {
+        console.error("Ошибка при скачивании файла", error);
+      });
   };
 
   return (
