28fe8714fe48858588a39e682edf37550c5739ee
MXkodo <nemchinow2018@mail.ru>
2024-08-22 20:22:28 +0400

    Some changes

--- src/components/DownloadButtom.jsx
+++ src/components/DownloadButtom.jsx
@@ -1,41 +1,71 @@
+import { useEffect } from "react";
 import axios from "axios";
 
-const DownloadButton = () => {
-  const handleDownload = () => {
-    const { WebApp } = window.Telegram;
-    WebApp.ready();
-    const user = WebApp.initDataUnsafe.user;
-    const chatID = user.id.toString();
+function AuthCheck({ setUserRole, setUserUUID }) {
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      const { WebApp } = window.Telegram;
 
-    console.log("Sending request with chatID:", chatID);
+      WebApp.ready();
+      const user = WebApp.initDataUnsafe.user;
+      const username = user ? user.username : null;
+      const chatID = WebApp.initDataUnsafe.chat.id;
 
-    axios({
-      url: `https://taskback.emivn.io/api/v1/tasks/download`,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: {
-        chatID: chatID,
-      },
-    })
-      .then((response) => {
-        console.log("Response:", response);
-        alert("Файл был отправлен вам в Telegram!");
-      })
-      .catch((error) => {
-        console.error("Ошибка при запросе к серверу", error);
-      });
-  };
+      if (username) {
+        axios
+          .post("https://taskauth.emivn.io/api/v1/user", {
+            username,
+          })
+          .then((response) => {
+            localStorage.setItem("authChecked", "true");
 
-  return (
-    <button
-      onClick={handleDownload}
-      className="bg-green-500 text-white py-2 px-4 rounded"
-    >
-      Экспорт
-    </button>
-  );
-};
+            const { role, uuid, chat_id } = response.data;
 
-export default DownloadButton;
+            setUserRole(role);
+            setUserUUID(uuid);
+
+            if (!chat_id && chatID) {
+              axios
+                .put(`https://taskauth.emivn.io/api/v1/users/chatid/${uuid}`, {
+                  chatID: chatID,
+                })
+                .then((response) => {
+                  console.log("Chat ID updated successfully");
+                })
+                .catch((error) => {
+                  console.error("Error updating chat ID:", error);
+                });
+            }
+          })
+          .catch((error) => {
+            console.error("Axios error:", error);
+            alert(
+              "Вас не зарегистрировали, вы не можете использовать приложение"
+            );
+
+            if (window.Telegram && window.Telegram.WebApp) {
+              window.Telegram.WebApp.close();
+            }
+          });
+      } else {
+        alert("Пользователь не найден в Telegram");
+
+        if (window.Telegram && window.Telegram.WebApp) {
+          window.Telegram.WebApp.close();
+        }
+      }
+    } else {
+      console.error("Telegram Web Apps API is not loaded");
+
+      alert("Произошла ошибка при загрузке приложения");
+
+      if (window.Telegram && window.Telegram.WebApp) {
+        window.Telegram.WebApp.close();
+      }
+    }
+  }, [setUserRole, setUserUUID]);
+
+  return null;
+}
+
+export default AuthCheck;
