8dc01bdb99a56e22ba5292b0df83224fb2378eb8
MXkodo <nemchinow2018@mail.ru>
2024-08-22 20:53:18 +0400

    Some changes

--- src/components/AuthCheck.jsx
+++ src/components/AuthCheck.jsx
@@ -9,17 +9,40 @@ function AuthCheck({ setUserRole, setUserUUID }) {
       WebApp.ready();
       const user = WebApp.initDataUnsafe.user;
       const username = user ? user.username : null;
+      const chatID = WebApp.initDataUnsafe.chat.id;
+
+      console.log("Telegram WebApp is ready.");
+      console.log("User:", user);
+      console.log("Username:", username);
+      console.log("Chat ID from WebApp:", chatID);
 
       if (username) {
         axios
-          .post("https://taskauth.emivn.io/api/v1/user", {
-            username,
-          })
+          .post("https://taskauth.emivn.io/api/v1/user", { username })
           .then((response) => {
+            const { role, uuid, chat_id } = response.data;
+
+            console.log("User data response:", response.data);
+
             localStorage.setItem("authChecked", "true");
 
-            setUserRole(response.data.role);
-            setUserUUID(response.data.uuid);
+            setUserRole(role);
+            setUserUUID(uuid);
+
+            // Если chat_id в ответе пустой и chatID существует, обновляем chat_id
+            if (!chat_id && chatID) {
+              console.log("Updating chat ID...");
+              axios
+                .put(`https://taskauth.emivn.io/api/v1/users/chatid/${uuid}`, {
+                  chat_id: chatID,
+                })
+                .then(() => {
+                  console.log("Chat ID updated successfully");
+                })
+                .catch((error) => {
+                  console.error("Error updating chat ID:", error);
+                });
+            }
           })
           .catch((error) => {
             console.error("Axios error:", error);
@@ -32,6 +55,7 @@ function AuthCheck({ setUserRole, setUserUUID }) {
             }
           });
       } else {
+        console.error("Username not found in Telegram");
         alert("Пользователь не найден в Telegram");
 
         if (window.Telegram && window.Telegram.WebApp) {
--- src/components/DownloadButtom.jsx
+++ src/components/DownloadButtom.jsx
@@ -1,86 +1,41 @@
-import { useEffect } from "react";
 import axios from "axios";
 
-function AuthCheck({ setUserRole, setUserUUID }) {
-  useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
-      const { WebApp } = window.Telegram;
+const DownloadButton = () => {
+  const handleDownload = () => {
+    const { WebApp } = window.Telegram;
+    WebApp.ready();
+    const user = WebApp.initDataUnsafe.user;
+    const chatID = user.id.toString();
 
-      WebApp.ready();
-      const user = WebApp.initDataUnsafe.user;
-      const username = user ? user.username : null;
-      const chatID = WebApp.initDataUnsafe.chat.id;
+    console.log("Sending request with chatID:", chatID);
 
-      alert("Telegram WebApp is ready.");
-      alert("User:", user);
-      alert("Username:", username);
-      alert("Chat ID:", chatID);
+    axios({
+      url: `https://taskback.emivn.io/api/v1/tasks/download`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        chatID: chatID,
+      },
+    })
+      .then((response) => {
+        console.log("Response:", response);
+        alert("Файл был отправлен вам в Telegram!");
+      })
+      .catch((error) => {
+        console.error("Ошибка при запросе к серверу", error);
+      });
+  };
 
-      if (username) {
-        axios
-          .post("https://taskauth.emivn.io/api/v1/user", {
-            username,
-          })
-          .then((response) => {
-            alert("User data response:", response.data);
+  return (
+    <button
+      onClick={handleDownload}
+      className="bg-green-500 text-white py-2 px-4 rounded"
+    >
+      Экспорт
+    </button>
+  );
+};
 
-            localStorage.setItem("authChecked", "true");
-
-            const { role, uuid, chat_id } = response.data;
-
-            alert("Role:", role);
-            alert("UUID:", uuid);
-            alert("Chat ID from response:", chat_id);
-
-            setUserRole(role);
-            setUserUUID(uuid);
-
-            // Обновляем chat_id, если оно пустое
-            if (!chat_id && chatID) {
-              alert("Updating chat ID...");
-              axios
-                .put(`https://taskauth.emivn.io/api/v1/users/chatid/${uuid}`, {
-                  chat_id: chatID, // Используем chat_id вместо chatID
-                })
-                .then(() => {
-                  alert("Chat ID updated successfully");
-                })
-                .catch((error) => {
-                  console.error("Error updating chat ID:", error);
-                });
-            }
-          })
-          .catch((error) => {
-            console.error("Axios error:", error);
-            alert(
-              "Вас не зарегистрировали, вы не можете использовать приложение"
-            );
-
-            if (window.Telegram && window.Telegram.WebApp) {
-              window.Telegram.WebApp.close();
-            }
-          });
-      } else {
-        console.error("Username not found in Telegram");
-
-        alert("Пользователь не найден в Telegram");
-
-        if (window.Telegram && window.Telegram.WebApp) {
-          window.Telegram.WebApp.close();
-        }
-      }
-    } else {
-      console.error("Telegram Web Apps API is not loaded");
-
-      alert("Произошла ошибка при загрузке приложения");
-
-      if (window.Telegram && window.Telegram.WebApp) {
-        window.Telegram.WebApp.close();
-      }
-    }
-  }, [setUserRole, setUserUUID]);
-
-  return null;
-}
-
-export default AuthCheck;
+export default DownloadButton;
