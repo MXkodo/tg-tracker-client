cfbfb030b2817d64f9d30c1dc06fae1755e9c9ea
MXkodo <nemchinow2018@mail.ru>
2024-08-22 21:16:16 +0400

    Some changes

--- src/components/AuthCheck.jsx
+++ src/components/AuthCheck.jsx
@@ -9,32 +9,40 @@ function AuthCheck({ setUserRole, setUserUUID }) {
       WebApp.ready();
       const user = WebApp.initDataUnsafe.user;
       const username = user ? user.username : null;
-      const chatID = user.id.toString();
-      console.log("Telegram WebApp is ready.");
-      console.log("User:", user);
-      console.log("Username:", username);
-      console.log("Chat ID from WebApp:", chatID);
+      const chatID = WebApp.initDataUnsafe.chat.id;
 
       if (username) {
         axios
-          .post("https://taskauth.emivn.io/api/v1/user", { username })
+          .post("https://taskauth.emivn.io/api/v1/user", {
+            username,
+          })
           .then((response) => {
-            const { role, uuid, chat_id } = response.data;
-
-            console.log("User data response:", response.data);
-
             localStorage.setItem("authChecked", "true");
+
+            const { role, uuid, chat_id } = response.data;
 
             setUserRole(role);
             setUserUUID(uuid);
 
+            console.log("Received chat_id from server:", chat_id);
+            console.log("Chat ID from WebApp:", chatID);
+
             // Если chat_id в ответе пустой и chatID существует, обновляем chat_id
             if (!chat_id && chatID) {
               console.log("Updating chat ID...");
+
+              // Подготовка данных для запроса
+              const dataToSend = {
+                chat_id: chatID, // отправляем как chat_id
+              };
+
+              console.log("Data to be sent:", dataToSend);
+
               axios
-                .put(`https://taskauth.emivn.io/api/v1/users/chatid/${uuid}`, {
-                  chat_id: chatID,
-                })
+                .put(
+                  `https://taskauth.emivn.io/api/v1/users/chatid/${uuid}`,
+                  dataToSend
+                )
                 .then(() => {
                   console.log("Chat ID updated successfully");
                 })
@@ -54,7 +62,6 @@ function AuthCheck({ setUserRole, setUserUUID }) {
             }
           });
       } else {
-        console.error("Username not found in Telegram");
         alert("Пользователь не найден в Telegram");
 
         if (window.Telegram && window.Telegram.WebApp) {
